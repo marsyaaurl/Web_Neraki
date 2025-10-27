be3b26d9a2ee5d2f3c4d196731788c9bd854a336
LetsTray <letstraey@gmail.com>
2025-10-27 16:04:56 +0700

    landing page done

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,27 +2,26 @@
 
 export default function Button({
   label,
-  variant,
-  size,
+  variant = "blue",
   className = "",
   ...props
 }) {
   const variants = {
-    blue: " bg-blue text-white hover:bg-blueHover hover:text-blue transition-color duration-150",
-    yellow: " bg-yellow text-black hover:bg-yellowHover hover:text-white",
-    red: " bg-red text-white transition-color duration-150 hover:bg-redHover hover:text-red",
+    blue: "bg-blue text-white hover:bg-blueHover hover:text-blue transition-colors duration-150",
+    yellow:
+      "bg-yellow text-black hover:bg-yellowHover hover:text-white transition-colors duration-150",
+    red: "bg-red text-white hover:bg-redHover hover:text-red transition-colors duration-150",
   };
 
-  const sizes = {};
-
-  const variantClass = variants[variant];
-  const sizeClass = sizes[size];
+  const variantClass = variants[variant] || variants.blue;
 
   return (
     <button
-      className={` text-lg font-semibold items-center rounded-full ${variantClass} ${className}`}
+      className={`text-lg font-semibold rounded-full items-center ${variantClass} ${className}`}
+      {...props}
     >
       {label}
     </button>
   );
 }
+
