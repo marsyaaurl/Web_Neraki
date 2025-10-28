bcc152fc3412daee960575bbcdc5f05322f00708
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-10-28 16:04:37 +0700

    add card component

--- frontend/src/app/Home/page.js
+++ frontend/src/app/Home/page.js
@@ -1,5 +1,6 @@
 import NavbarLoggedIn from "@/components/NavbarLoggedIn";
 import Map from "@/components/Map";
+import UmkmList from "@/components/UmkmList";
 
 export default function Home() {
     return (
@@ -10,7 +11,12 @@ export default function Home() {
             <div className="pt-20 px-5 flex flex-col">
                 <Map />
             </div>
-            <div className="px-5 py-5">
+            <div className="px-5 py-20 w-full h-64 flex flex-col text-center">
+                <h1 className="text-3xl font-bold text-blue mb-2">UMKM</h1>
+                <h4 className="text-blue font-light text-base sm:text-lg">
+                    Temukan usaha lokal yang kamu butuhkan!
+                </h4>
+                <UmkmList />
             </div>
         </>
     )
