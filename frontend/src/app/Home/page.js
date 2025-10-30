84b3e9a612a8bec9a921cff4a7e8b7bee16117a1
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-10-30 08:42:11 +0700

    umkm card components

--- frontend/src/app/Home/page.js
+++ frontend/src/app/Home/page.js
@@ -1,22 +1,38 @@
 import NavbarLoggedIn from "@/components/NavbarLoggedIn";
 import Map from "@/components/Map";
 import UmkmList from "@/components/UmkmList";
+import Filter from "@/components/Filter";
 
 export default function Home() {
     return (
         <>
+            {/* Navbar */}
             <div>
                 <NavbarLoggedIn />
             </div>
-            <div className="pt-20 px-5 flex flex-col">
+
+            {/* Map */}
+            <div className="pt-28 px-5 flex flex-col">
                 <Map />
             </div>
-            <div className="px-5 py-20 w-full h-64 flex flex-col text-center">
-                <h1 className="text-3xl font-bold text-blue mb-2">UMKM</h1>
-                <h4 className="text-blue font-light text-base sm:text-lg">
-                    Temukan usaha lokal yang kamu butuhkan!
-                </h4>
-                <UmkmList />
+
+            {/* Perfect Kit Date and UMKM Recommendations*/}
+            <div>
+                
+            </div>
+
+            {/* List of UMKM */}
+            <div className="px-5 py-20 w-full">
+                <div className="text-center mb-8">
+                    <h1 className="text-3xl font-bold text-blue mb-2">UMKM</h1>
+                    <h4 className="text-blue font-light text-base sm:text-lg">
+                        Temukan usaha lokal yang kamu butuhkan!
+                    </h4>
+                </div>
+                <div className="flex flex-col lg:flex-row gap-6 w-full max-w-7xl mx-auto">
+                    <Filter />
+                    <UmkmList />
+                </div>
             </div>
         </>
     )
