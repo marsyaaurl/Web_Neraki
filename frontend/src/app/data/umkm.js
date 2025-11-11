b30f0eea029b6fbc46b79f7492f90c499cba2902
LetsTray <letstraey@gmail.com>
2025-11-11 16:10:58 +0700

    on progress search feature

--- frontend/src/app/data/umkm.js
+++ frontend/src/app/data/umkm.js
@@ -12,6 +12,15 @@ import Pannatown3 from "../../../public/assets/images/Pannatown3.jpg";
 import Pannatown4 from "../../../public/assets/images/Pannatown4.jpg";
 import Pannatown5 from "../../../public/assets/images/Pannatown5.jpg";
 import Pannatown6 from "../../../public/assets/images/Pannatown6.jpg";
+import Niekting from "../../../public/assets/images/Niekting.jpg";
+import Niekting1 from "../../../public/assets/images/Niekting1.jpg";
+import Niekting2 from "../../../public/assets/images/Niekting2.jpg";
+import Niekting3 from "../../../public/assets/images/Niekting3.jpg";
+import Niekting4 from "../../../public/assets/images/Niekting4.jpg";
+import Niekting5 from "../../../public/assets/images/Niekting5.jpg";
+import Niekting6 from "../../../public/assets/images/Niekting6.jpg";
+import Niekting7 from "../../../public/assets/images/Niekting7.jpg";
+import Niekting8 from "../../../public/assets/images/Niekting8.jpg";
 
 export const umkm = [
   {
@@ -20,20 +29,17 @@ export const umkm = [
     owner: "Salfiah",
     category: "Craft",
     shortloc: "Penjaringan, Jakarta Utara",
-    location:
-      "Jl. Petak Asem Selatan No. 23, Penjaringan, Jakarta Utara 14440",
+    location: "Jl. Petak Asem Selatan No. 23, Penjaringan, Jakarta Utara 14440",
     contact: {
       whatsapp: "088210983065",
       instagram: "waroengrajutfiah",
     },
     description:
       "Menjual berbagai macam aneka kerajinan tangan rajut, seperti gantungan kunci, bando, bros, tas, dan lain-lain.",
-    priceRange: "",
+    priceRange: "25.000-35.000",
     backStory:
       "Perjalanan usaha rajut saya dimulai dari emperan Fatahillah, Kota Tua, tahun 2010. Di tengah hiruk-pikuk wisatawan dan suasana heritage, saya menjajakan kerajinan tangan dengan penuh semangat. Lima tahun kemudian, saya bergabung sebagai UKM binaan Dekranasda—sebuah langkah besar yang membuka banyak pintu pembinaan dan peluang. Namun, jalan usaha tak selalu mulus. Pandemi COVID-19 memaksa saya berhenti selama tiga tahun. Di waktu yang sama, saya menjalani peran baru sebagai ibu dari dua anak, masing-masing lahir dengan jeda tiga tahun. Fokus saya pun bergeser, dan selama itu saya tidak aktif mengikuti bazar atau kegiatan UKM. Sebelumnya, saya sempat mendapat kios dari Walikota Jakarta Utara di Mangga Dua Pasar Pagi lantai 5, tapi hanya bertahan satu tahun karena tidak diperpanjang oleh Sudin. Saya juga pernah mengelola kios di Fatahillah selama 1,5 tahun, namun harus berhenti karena kesibukan anak masuk sekolah. Kini, setelah melewati berbagai fase kehidupan, saya kembali aktif. Dengan semangat baru dan pengalaman panjang, saya siap melanjutkan usaha rajut ini—membawa cerita, warna, dan kehangatan dalam setiap karya.",
-    photos: [
-      WaroengRajutFiah1, WaroengRajutFiah2
-    ],
+    photos: [WaroengRajutFiah1, WaroengRajutFiah2],
     atmospherePhotos: [],
     mood: {
       energy: ["medium"],
@@ -48,19 +54,23 @@ export const umkm = [
     owner: "Tirza",
     category: "Makanan",
     shortloc: "Cakung, Jakarta Timur",
-    location:
-      "",
+    location: "",
     contact: {
-      whatsapp: "088210983065",
-      instagram: "waroengrajutfiah",
+      whatsapp: "081398877766",
+      instagram: "pannatown",
     },
     description:
       "Pannatown adalah usaha dessert dan minuman yang bisa dipesan online maupun untuk acara spesial. Kami juga menyediakan dessert table cantik untuk wedding, ulang tahun, dan berbagai event lainnya—siap mempermanis momen berharga Anda.",
-    priceRange: "",
+    priceRange: "5.000-300.000",
     backStory:
       "Pannatown lahir di tahun 2019, bukan dari rencana besar, tapi dari titik balik kehidupan. Saat pandemi melanda dan usaha pertama saya terpaksa berhenti, saya memilih untuk bangkit lewat hal yang sederhana: makanan penutup dan minuman yang bisa menghadirkan kebahagiaan kecil di tengah masa sulit. Berawal dari dapur rumah, saya mulai menjual dessert secara online. Tak disangka, dukungan datang dari berbagai arah—termasuk dari program UMKM pemerintah yang sangat membantu saya bertahan dan berkembang. Pannatown pun resmi terdaftar sebagai UMKM, dan sejak itu, saya merasa benar-benar diberkati. Banyak yang percaya, banyak yang mendukung. Kini, Pannatown tak hanya hadir secara online, tapi juga mempermanis berbagai acara lewat dessert table untuk wedding, ulang tahun, dan event lainnya. Dari masa jatuh, lahirlah sesuatu yang manis.",
     photos: [
-      Pannatown1, Pannatown2, Pannatown3, Pannatown4, Pannatown5, Pannatown6
+      Pannatown1,
+      Pannatown2,
+      Pannatown3,
+      Pannatown4,
+      Pannatown5,
+      Pannatown6,
     ],
     atmospherePhotos: [],
     mood: {
@@ -69,6 +79,43 @@ export const umkm = [
       setting: ["indoor"],
     },
     tags: ["handmade", "craft", "local-product"],
+  },
+  {
+    image: Niekting,
+    name: "Niekting",
+    owner: " Inna Sri Sugiati",
+    category: "Makanan",
+    shortloc: "Pancoran, Jakarta Selatan",
+    location:
+      "Jl. Pancoran Timur Raya No. 50, RT. 006 / RW. 008, Kelurahan Pengadegan, Kecamatan Pancoran, Jakarta Selatan 12770",
+    contact: {
+      whatsapp: "081284668146",
+      instagram: "asinanfermentasiniektingjaksel",
+    },
+    description:
+      "Asinan Fermentasi Niekting adalah usaha kuliner yang mengusung konsep fermentasi alami dalam sajian asinan, menghadirkan sensasi dan kesehatan di setiap gigitannya.",
+    priceRange: "",
+    backStory:
+      "Berawal dari dapur rumahan dan keresahan akan makanan sehat yang minim pengawet, Niekting lahir sebagai pelopor asinan fermentasi alami yang tak hanya memanjakan lidah, tapi juga menjaga kesehatan pencernaan. Mengusung resep tradisional turun-temurun yang dikombinasikan dengan sentuhan modern dan inovatif, Niekting menyajikan perpaduan segar antara buah dan sayur dalam kemasan yang praktis dan penuh manfaat probiotik.",
+    photos: [
+      Niekting1,
+      Niekting2,
+      Niekting3,
+      Niekting4,
+      Niekting5,
+      Niekting6,
+      Niekting7,
+      Niekting8,
+    ],
+    atmospherePhotos: [],
+    mood: {
+      energy: ["medium", "high"],
+      vibe: ["social", "creative"],
+      setting: ["indoor"],
+    },
+    tags: ["comfort-food", "casual", "hearty"],
+    dateTypes: ["romantic", "adventure", "foodie", "chill"],
+    mealTime: ["lunch", "dinner", "brunch"],
   },
   {
     image: NasiKokoh,
